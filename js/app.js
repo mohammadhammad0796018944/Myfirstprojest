7ab3f62e752e47e9f0e311c52954d76a2e5e44ef
mohammad issa mahmoud hammad <wvwv@yahoo.com>
2021-02-23 17:07:28 +0200

    farah playtion

---
+++ js/app.js
@@ -0,0 +1,22 @@
+confirm("push to see massage");
+
+
+var numberofgroupplayer= prompt("enter number of player");
+var ageplayer= prompt("enter your age");
+
+
+
+if (ageplayer>18){
+    alert( "you are allowe to play!");
+    
+    }else{
+        alert("you have to go");
+    }
+
+if (numberofgroupplayer>4) {
+    alert("please kepp going");
+}
+
+
+
+
