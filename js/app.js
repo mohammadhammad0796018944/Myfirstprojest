033b0ee7dc7dc7716f9ae59bac972fffa8cf290b
mohammadhammad0796018944 <79259784+mohammadhammad0796018944@users.noreply.github.com>
2021-02-24 14:21:07 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ var numberofgroupplayer= prompt("enter number of player");
 var ageplayer= prompt("enter your age");
 
 
-if (numberofgroupplayer <4) {
+if (numberofgroupplayer<4) {
     alert("please you can play");
     
     }else{
         alert("you cant play just for player");
 
         
-if (ageplayer >18){
+if (ageplayer>18){
     alert( "you are allowe to play!");
     
     }else{
