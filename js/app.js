e0123c4262011678c236f2ad2dd501894715e16e
mohammad issa mahmoud hammad <wvwv@yahoo.com>
2021-02-24 14:58:35 +0200

    java update

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@ confirm("push to see massage");
 
 
 var numberofgroupplayer= prompt("enter number of player");
+var ageplayer= prompt("enter your age");
 
-
-if (numberofgroupplayer<4) {
+if (numberofgroupplayer<=4) {
     alert("please you can play");
     
     }else{
         alert("you cant play just 4 player or less");
 
 
-    }var ageplayer= prompt("enter your age");
+    }
 
 if (ageplayer>18){
     alert( "you are allowe to play!");
