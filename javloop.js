07d4747e0512939b5b6179c30fabb1ac843f95a0
mohammad issa mahmoud hammad <wvwv@yahoo.com>
2021-02-24 22:57:01 +0200

    playstion

---
+++ javloop.js
@@ -0,0 +1,37 @@
+confirm("push to see massage");
+
+
+
+
+
+
+
+var favgame = "fight"; 
+
+while(enterthefavgame != "fight")
+{
+var enterthefavgame = prompt("guess the fav game");
+counter=0;
+alert("please try again")
+counter++
+}
+alert("congrats the correct name is " + favgame  );
+console.log( "congrats the correct name is " + favgame  );
+
+
+
+var numberofimg = prompt( "enter number of img you would show it " );
+var storenumberofimg = numberofimg;
+
+for (var i =0 ; i<storenumberofimg ; i++)
+    {
+
+        document.write('<img src="">');
+    }
+
+
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -2,16 +2,43 @@ confirm("push to see massage");
 
 
 var numberofgroupplayer= prompt("enter number of player");
-var ageplayer= prompt("enter your age");
+var ageplayer=   prompt("enter your age");
+var roomplayer= prompt("which room you need");
+
+
+
 
 if (numberofgroupplayer <=4 ) {
     alert("please you can play");
     
     }else{
         alert("you cant play just 4 player or less");
+}
 
-    }
 
+if (roomplayer =1 ) {
+    alert("unlimted room time");
+    document.write('<img src="https://i2-prod.mirror.co.uk/incoming/article22653490.ece/ALTERNATES/s615b/0_FIFA-21-pre-order-mirror.jpg">')
+
+    }else{
+        alert("you choic the hour room time");
+        document.write('<img src="https://i.ytimg.com/vi/sbYv94pAOoI/maxresdefault.jpg">')
+     
+}                                                           
+
+
+if (roomplayer =2 ) {
+    alert("hour room time");
+    document.write('<img src="https://i.ytimg.com/vi/sbYv94pAOoI/maxresdefault.jpg">')
+
+
+    
+    }else{
+        alert("you choic the unlimted room time");
+        document.write('<img src="https://i2-prod.mirror.co.uk/incoming/article22653490.ece/ALTERNATES/s615b/0_FIFA-21-pre-order-mirror.jpg">')
+     
+}
+ 
 
 
 
